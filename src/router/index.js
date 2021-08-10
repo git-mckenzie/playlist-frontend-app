8f3eefce4fdd9029e7fd56a55abebdb01b5fbe6a
Jason McKenzie <jmac.atx@gmail.com>
2021-08-10 15:37:15 -0500

    configs axios and adds initial routes in index.js for Home, Signup, Login/Out, UserPage, Playlist Index, New, Show and Edit pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,28 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+// import Signup from "../views/Signup.vue";
+// import Login from "../views/Login.vue";
+// import Logout from "../views/Logout.vue";
+// import UserPage from "../views/UserPage.vue";
+// import PlaylistsIndex from "../views/PlaylistsIndex.vue";
+// import PlaylistsNew from "../views/PlaylistsNew.vue";
+// import PlaylistsShow from "../views/PlaylistsShow.vue";
+// import PlaylistsEdit from "../views/PlaylistsEdit.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
+  { path: "/", name: "Home", component: Home },
+  // { path: "/signup", name: "signup", component: Signup },
+  // { path: "/login", name: "login", component: Login },
+  // { path: "/logout", name: "logout", component: Logout },
+  // { path: "/userpage", name: "user-page", component: UserPage },
+  // { path: "/playlists", name: "playlists-index", component: PlaylistsIndex },
+  // { path: "/playlists/new", name: "playlists-new", component: PlaylistsNew },
+  // { path: "/playlists/:id", name: "playlists-show", component: PlaylistsShow },
+  // { path: "/playlists", name: "playlists-edit", component: PlaylistsEdit },
+  { path: "/about", name: "About", component: () => import(/* webpackChunkName: "about" */ "../views/About.vue") },
 ];
 
 const router = new VueRouter({
