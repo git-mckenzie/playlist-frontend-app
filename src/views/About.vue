51be2fe6e290361dbf29d43300c135232aeb1b7e
Jason McKenzie <jmac.atx@gmail.com>
2021-08-16 13:15:34 -0500

    removed PlaylistsIndex.vue and updated copy on About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>CampFyre - Bandcamp Playlist app</h1>
+    <p>
+      CampFyre is a Bandcamp playlist creator, designed to help bands and artists make more money, by making it easier
+      for music fans to discover, share and buy new music.
+    </p>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,39 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <h1>All Campfire Playlists</h1>
+    <div v-for="playlist in playlists" :key="playlist.id">
+      <router-link :to="`/playlists/${playlist.id}`">
+        <h2>{{ playlist.playlist_name }}</h2>
+      </router-link>
+      <!-- <router-link :to="`/users/${playlist.user_id}`">
+        <h3>{{ playlist.user.user_name }}</h3>
+      </router-link> -->
+    </div>
   </div>
 </template>
 
 <style></style>
 
 <script>
+import axios from "axios";
 export default {
+  name: "App",
+  components: {},
   data: function () {
     return {
       message: "Welcome to Vue.js!",
     };
   },
-  created: function () {},
-  methods: {},
+  created: function () {
+    this.playlistsIndex();
+  },
+  methods: {
+    playlistsIndex: function () {
+      axios.get("/playlists").then((response) => {
+        console.log(response.data);
+        this.posts = response.data;
+      });
+    },
+  },
 };
 </script>
