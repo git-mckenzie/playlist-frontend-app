1487222a54cdfac8736a4b1202168b7714a8b19b
Jason McKenzie <jmac.atx@gmail.com>
2021-08-20 13:54:17 -0500

    adds ability for user to logout

---
+++ src/views/Logout.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="logout"></div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  created: function () {
+    delete axios.default.headers.common["Authorization"];
+    localStorage.removeItem("jwt");
+    localStorage.removeItem("user_id");
+  },
+};
+</script>
